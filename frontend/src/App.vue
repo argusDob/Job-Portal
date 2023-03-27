642b6a6f23c33c3af4bb5028e3b521f8baa5b206
ioannis <ioannis.dimitriou@emark.com>
2023-03-27 11:43:41 +0200

    remove-unsecessery-code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <h1>Ioannis</h1>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </nav>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "./assets/scss/colors.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: $background-color;
+  height: 100vh;
 }
 
 nav {
