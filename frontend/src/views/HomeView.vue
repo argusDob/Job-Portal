e5de2fc3d9239e448467a38b9c910965b424f5bc
ioannis <ioannis.dimitriou@emark.com>
2023-03-27 15:29:26 +0200

    wip

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -2,14 +2,19 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <RegistrationForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import RegistrationForm from "../components/RegistrationForm/RegistrationForm.vue";
 
 export default {
   name: "HomeView",
+  components: {
+    RegistrationForm,
+  },
 };
 </script>
