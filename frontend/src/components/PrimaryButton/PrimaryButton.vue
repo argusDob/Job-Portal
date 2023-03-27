3e4531281491ea9ed3a0ef5dfa620451f658eceb
ioannis <ioannis.dimitriou@emark.com>
2023-03-27 15:28:43 +0200

    primary-button-implementation

---
+++ frontend/src/components/PrimaryButton/PrimaryButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="primary-button typography">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./PrimaryButton.scss"></style>
