b6e80a67db145b5d2c2b0e5a1d3454d451386108
ioannis <ioannis.dimitriou@emark.com>
2023-03-27 15:28:30 +0200

    vlidation-input-text

--- frontend/src/components/InputText/InputText.vue
+++ frontend/src/components/InputText/InputText.vue
@@ -3,50 +3,80 @@
     <label :for="id" class="input-text__label">{{ label }}</label>
     <input
       :id="id"
-      :type="inputType"
+      :type="type"
       class="input-text__field"
       :placeholder="placeholder"
+      :name="name"
       :maxlength="maxlength"
       :minlength="minlength"
       :autocomplete="autocomplete"
       :required="required"
       v-model="valueData"
+      @keyup="handleValue"
     />
   </div>
 </template>
 
 <script>
+import { useVuelidate } from "@vuelidate/core";
+import { required, minLength } from "@vuelidate/validators";
+
 export default {
   props: {
+    autocomplete: {
+      type: Boolean,
+    },
     label: {
       type: String,
-      required: true,
+      //   required: true,
     },
     maxlength: {
       type: Number,
-      required: true,
+      //   required: true,
     },
     minlength: {
       type: Number,
-      required: true,
+      //   required: true,
+    },
+    name: {
+      type: String,
     },
     placeholder: {
       type: String,
-      required: true,
+      //   required: true,
+    },
+    required: {
+      type: Boolean,
     },
     type: {
       type: String,
-      required: true,
+      //   required: true,
     },
+  },
+  setup() {
+    return { rules: useVuelidate() };
   },
   data() {
     return {
       valueData: "",
     };
   },
+  validations() {
+    return {
+      valueData: { minLength: minLength(this.minlength), required }, // Matches this.firstName
+    };
+  },
   computed: {
     id() {
       return "input-" + Math.random().toString(36).substr(2, 9);
+    },
+  },
+  methods: {
+    handleValue() {
+      const validationReport = {};
+      (validationReport.value = this.valueData),
+        (validationReport.isInvalid = this.rules.$invalid);
+      this.$emit("change", validationReport);
     },
   },
 };
