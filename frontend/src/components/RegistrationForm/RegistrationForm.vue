3bfc9d2c26d474b92ac9f7a57ec0d1bdd8867fa2
ioannis <ioannis.dimitriou@emark.com>
2023-03-28 16:21:53 +0200

    form-validation

--- frontend/src/components/RegistrationForm/RegistrationForm.vue
+++ frontend/src/components/RegistrationForm/RegistrationForm.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="registration-form">
+  <div class="registration-form typography">
     <div class="registration-form__container">
-      <div v-for="(field, index) in fields" :key="index">
-        <InputText v-bind="field" @change="handleChange" />
-      </div>
-      <div class="registration-form__submit-button">
-        <SubmitButton text="Submit" />
-      </div>
+      <h3>Register and unlock more features</h3>
+      <form @submit.prevent="submit">
+        <div v-for="(field, index) in fields" :key="index">
+          <InputText
+            v-bind="field"
+            :index="index"
+            @change="handleChange"
+            @blur="handleBlur"
+          />
+        </div>
+        <div class="registration-form__submit-button">
+          <SubmitButton text="Submit" />
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -25,12 +33,37 @@ export default {
   props: {},
   data() {
     return {
-      fields: getFormsConfig(),
+      fields: [...getFormsConfig()],
+      formValidationReport: [],
     };
   },
   methods: {
     handleChange(value) {
       console.log(value);
+      const existingIndex = this.formValidationReport.findIndex(
+        (item) => item.name === value.name
+      );
+      if (existingIndex >= 0) {
+        this.formValidationReport.splice(existingIndex, 1, value);
+      } else {
+        this.formValidationReport.push(value);
+      }
+    },
+    handleBlur(validationReport) {
+      this.fields[validationReport.inputIndex].isValid =
+        !validationReport.isInvalid;
+    },
+    isFormValid() {
+      let isValid = false;
+      this.formValidationReport.forEach((element) => {
+        isValid = !element.isInvalid;
+      });
+      return isValid;
+    },
+    submit() {
+      if (this.isFormValid()) {
+        console.log("sumbit");
+      }
     },
   },
 };
