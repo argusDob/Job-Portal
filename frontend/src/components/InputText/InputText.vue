5143e47a341937f24761d6e1515b95a2c2692b4e
ioannis <ioannis.dimitriou@emark.com>
2023-03-28 16:21:35 +0200

    validation

--- frontend/src/components/InputText/InputText.vue
+++ frontend/src/components/InputText/InputText.vue
@@ -1,57 +1,78 @@
 <template>
-  <div class="input-text typography">
+  <div class="input-text">
     <label :for="id" class="input-text__label">{{ label }}</label>
     <input
       :id="id"
       :type="type"
       class="input-text__field"
+      :class="{
+        'input-text__field-valid': isInputValid,
+        'input-text__field-invalid': isInputValid === false,
+      }"
       :placeholder="placeholder"
       :name="name"
       :maxlength="maxlength"
       :minlength="minlength"
       :autocomplete="autocomplete"
       :required="required"
+      :index="index"
       v-model="valueData"
-      @keyup="handleValue"
+      @keyup="handleValue()"
+      @blur="handleBlur"
     />
+    <SoftError v-if="isInputValid === false">{{ errorMessage }}</SoftError>
   </div>
 </template>
 
 <script>
 import { useVuelidate } from "@vuelidate/core";
-import { required, minLength } from "@vuelidate/validators";
+import { required, minLength, email } from "@vuelidate/validators";
+
+import SoftError from "../SoftError/SoftError.vue";
 
 export default {
   props: {
     autocomplete: {
       type: Boolean,
     },
+    errorMessage: {
+      type: String,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+    isValid: {
+      type: Boolean,
+      default: null,
+    },
     label: {
       type: String,
-      //   required: true,
+      required: true,
     },
     maxlength: {
       type: Number,
-      //   required: true,
     },
     minlength: {
       type: Number,
-      //   required: true,
     },
     name: {
       type: String,
+      required: true,
     },
     placeholder: {
       type: String,
-      //   required: true,
     },
     required: {
       type: Boolean,
     },
     type: {
       type: String,
-      //   required: true,
+      required: true,
     },
+  },
+  components: {
+    SoftError,
   },
   setup() {
     return { rules: useVuelidate() };
@@ -59,9 +80,15 @@ export default {
   data() {
     return {
       valueData: "",
+      validationReport: {},
     };
   },
   validations() {
+    if (this.name === "email") {
+      return {
+        valueData: { minLength: minLength(this.minlength), required, email },
+      };
+    }
     return {
       valueData: { minLength: minLength(this.minlength), required }, // Matches this.firstName
     };
@@ -70,13 +97,29 @@ export default {
     id() {
       return "input-" + Math.random().toString(36).substr(2, 9);
     },
+    isInputValid() {
+      return this.isValid;
+    },
   },
   methods: {
     handleValue() {
-      const validationReport = {};
-      (validationReport.value = this.valueData),
-        (validationReport.isInvalid = this.rules.$invalid);
-      this.$emit("change", validationReport);
+      this.contstructalidationReport();
+      this.$emit("change", this.validationReport);
+    },
+    handleBlur() {
+      this.contstructalidationReport();
+
+      if (
+        Object.prototype.hasOwnProperty.call(this.validationReport, "isInvalid")
+      ) {
+        this.$emit("blur", this.validationReport);
+      }
+    },
+    contstructalidationReport() {
+      (this.validationReport.value = this.valueData),
+        (this.validationReport.isInvalid = this.rules.$invalid);
+      this.validationReport.inputIndex = this.index;
+      this.validationReport.name = this.name;
     },
   },
 };
