cad19d8f3ea087cdf2804c07764e5fad45c3e4c1
ioannis <ioannis.dimitriou@emark.com>
2023-03-28 16:21:08 +0200

    login-view

---
+++ frontend/src/views/Login/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login-view">
+    <div class="login-view__form">
+      <RegistrationForm />
+    </div>
+    <div class="login-view__registration">
+      <LoginForm />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import LoginForm from "../../components/LoginForm/LoginForm.vue";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm.vue";
+
+export default {
+  name: "LoginView",
+  components: {
+    RegistrationForm,
+    LoginForm,
+  },
+};
+</script>
+<style lang="scss" src="./LoginView.scss" scoped></style>
