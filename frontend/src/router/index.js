d652e2ca7b819a7b1657780f9bd9f17a43710b6d
ioannis <ioannis.dimitriou@emark.com>
2023-03-28 16:24:00 +0200

    login-view-lazy-load

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
   {
     path: "/login",
     name: "login",
-    component: () => import("../views/LoginView.vue"),
+    component: () => import("../views/Login/LoginView.vue"),
   },
 ];
 
