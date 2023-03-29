098f37b23f2ed6ad84ed1377125c87d23e35ef30
ioannis <ioannis.dimitriou@emark.com>
2023-03-29 11:28:46 +0200

    set-up-icons

---
+++ frontend/src/assets/icons/IconAction/IconAction.vue
@@ -0,0 +1,38 @@
+<template>
+  <span class="icon-action">
+    <b-icon
+      :icon="icon"
+      :animation="animation"
+      :font-scale="fontScale"
+      @click="handleIconClick"
+      tabindex="0"
+      role="button"
+    ></b-icon>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    animation: {
+      type: String,
+      required: false,
+    },
+    fontScale: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      require: true,
+    },
+  },
+  methods: {
+    handleIconClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./IconAction.scss"></style>
--- frontend/src/assets/icons/SpinIcon.vue
+++ frontend/src/assets/icons/SpinIcon.vue
@@ -1,3 +1,4 @@
 <template>
   <b-icon icon="three-dots" animation="cylon" font-scale="4"></b-icon>
 </template>
+//TODO DELETE AFTER PRIMARY BUTTON IMPLEMENTATION.
