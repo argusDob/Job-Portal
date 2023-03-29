349f042c2c3fa51d747fb0e7454ee5944b17594f
ioannis <ioannis.dimitriou@emark.com>
2023-03-29 11:30:08 +0200

    registration-form-config

---
+++ frontend/src/components/RegistrationForm/RegistrationFormConfig/registration.form.config.js
@@ -0,0 +1,33 @@
+export default function getFormsConfig() {
+  const fields = {
+    emailProps: {
+      type: "email",
+      format: "email",
+      label: "Email address:",
+      name: "email",
+      minlength: 10,
+      isValid: null,
+      errorMessage: "Email is required",
+    },
+    confirmPasswordProps: [
+      {
+        type: "password",
+        label: "Password:",
+        name: "Password",
+        minlength: 10,
+        isValid: null,
+        errorMessage: "Password is required",
+      },
+      {
+        type: "password",
+        label: "Confirm password:",
+        name: "Password1",
+        minlength: 10,
+        isValid: null,
+        errorMessage: "Confirmation Password is required",
+      },
+    ],
+  };
+
+  return fields;
+}
