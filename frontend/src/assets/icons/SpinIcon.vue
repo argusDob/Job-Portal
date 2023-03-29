2c447fbbad35e7cbed5e130860b7514fbd4b2ad4
ioannis <ioannis.dimitriou@emark.com>
2023-03-29 09:09:42 +0200

    primary-button-loading-state

---
+++ frontend/src/assets/icons/SpinIcon.vue
@@ -0,0 +1,3 @@
+<template>
+  <b-icon icon="three-dots" animation="cylon" font-scale="4"></b-icon>
+</template>
--- frontend/src/components/PrimaryButton/PrimaryButton.vue
+++ frontend/src/components/PrimaryButton/PrimaryButton.vue
@@ -1,13 +1,34 @@
 <template>
-  <button class="primary-button typography">{{ text }}</button>
+  <button class="primary-button typography">
+    <SpinIcon v-if="isLoading" />
+    <span v-else>{{ text }}</span>
+  </button>
 </template>
 
 <script>
+import SpinIcon from "../../assets/icons/SpinIcon.vue";
+
 export default {
+  components: {
+    SpinIcon,
+  },
   props: {
     text: {
       type: String,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    primaryButtonLoadingStart() {
+      this.isLoading = true;
+    },
+    primaryButtonLoadingFinish() {
+      this.isLoading = true;
     },
   },
 };
