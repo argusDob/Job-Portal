28296d6abe77c9e2ab44f866efb35605376fa6e7
ioannis <ioannis.dimitriou@emark.com>
2023-03-30 11:01:30 +0200

    implement-radio-group-component

---
+++ frontend/src/components/InputRadioGroup/InputRadioGroup.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="input-radio-group" :class="additionalClasses">
+    <div
+      class="input-radio-group--item"
+      v-for="(option, index) in options"
+      :key="index"
+    >
+      <input
+        :id="'radio-' + index"
+        :name="option.name"
+        type="radio"
+        :value="option.value"
+        v-model="selectedValue"
+        @change="handleChange"
+      />
+      <label :for="'radio-' + index" class="radio-label">{{
+        option.label
+      }}</label>
+      <SoftError></SoftError>
+    </div>
+  </div>
+</template>
+
+<script>
+import SoftError from "../SoftError/SoftError.vue";
+export default {
+  components: {
+    SoftError,
+  },
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    directionColumn: {
+      type: Boolean,
+      require: false,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      selectedValue: this.options.length > 0 ? this.options[0].value : "",
+    };
+  },
+  computed: {
+    additionalClasses() {
+      let classes = [];
+      if (this.directionColumn) classes.push("input-radio-group--column");
+      return classes;
+    },
+  },
+  //TODO IMPLEMENT VALIDATION
+  methods: {
+    handleChange() {
+      this.$emit("change", this.selectedValue);
+    },
+    getSelectedValue() {
+      return this.selectedValue;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./InputRadioGroup.scss"></style>
