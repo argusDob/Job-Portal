6ea8a5e6b060bc1ceb94303bdff7a89c44fc6318
ioannis <ioannis.dimitriou@emark.com>
2023-04-13 16:08:36 +0200

    import navbar as component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,24 @@
 <template>
   <div id="app">
-    <b-navbar type="dark" variant="dark">
-      <b-navbar-nav>
-        <b-nav-item href="#">Home</b-nav-item>
-
-        <!-- Navbar dropdowns -->
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown text="User" right>
-          <b-dropdown-item href="#">Account</b-dropdown-item>
-          <b-dropdown-item href="#"
-            ><router-link to="/Login">Login</router-link></b-dropdown-item
-          >
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
-    </b-navbar>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from "./components/NavBar/NavBar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 <style lang="scss">
 @import "./assets/scss/colors.scss";
 
 #app {
   background-color: $background-color;
   height: 100vh;
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    // font-weight: bold;
-    // color: #2c3e50;
-
-    // &.router-link-exact-active {
-    //   color: #42b983;
-    // }
-  }
 }
 </style>
