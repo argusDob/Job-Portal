c1a58a72c4456ff6fcfd9d3ae137dadae0f33b00
ioannis <ioannis.dimitriou@emark.com>
2023-04-13 16:11:42 +0200

    nav-bar-init-config

---
+++ frontend/src/components/NavBar/NavBarList.config.js
@@ -0,0 +1,22 @@
+export default function getNavBarListConfig() {
+  const list = [
+    {
+      title: "Search Job Openings",
+      pathTo: "/Home",
+    },
+    {
+      title: "Login",
+      pathTo: "/Login",
+    },
+    {
+      title: "Register",
+      pathTo: "/Login",
+    },
+    {
+      title: "Logout",
+      pathTo: "",
+    },
+  ];
+
+  return list;
+}
