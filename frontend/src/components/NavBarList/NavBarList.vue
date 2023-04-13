026b346b74418b964d58bd944c0bf825afba8385
ioannis <ioannis.dimitriou@emark.com>
2023-04-13 16:12:17 +0200

    navBar-list-implementation

---
+++ frontend/src/components/NavBarList/NavBarList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-bar-list">
+    <div
+      v-for="(item, index) in list"
+      :key="index"
+      class="nav-bar-list__item"
+      @click="handleClick()"
+    >
+      <router-link :to="item.pathTo">
+        <span>{{ item.title }}</span>
+      </router-link>
+      <b-icon icon="chevron-right"></b-icon>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    handleClick() {
+      this.$emit("navListClick");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped src="./NavBarList.scss"></style>
