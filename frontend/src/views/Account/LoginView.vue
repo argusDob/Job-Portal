53683525310aa5abbcb7892bf1e0f6c9689d4302
ioannis <ioannis.dimitriou@emark.com>
2023-04-14 11:08:14 +0200

    refactor-level

---
+++ frontend/src/views/Account/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login-view">
+    <div class="login-view__form">
+      <RegistrationForm />
+    </div>
+    <div class="login-view__registration">
+      <LoginForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from "../../components/LoginForm/LoginForm.vue";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm.vue";
+
+export default {
+  name: "LoginView",
+  components: {
+    RegistrationForm,
+    LoginForm,
+  },
+};
+</script>
+<style lang="scss" src="./LoginView.scss" scoped></style>
