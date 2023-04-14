83c120ac6372a02c1dee0ae0525e481764022b12
ioannis <ioannis.dimitriou@emark.com>
2023-04-14 11:07:05 +0200

    implement-link-action-component

---
+++ frontend/src/components/LinkAction/LinkAction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="link-action">
+    <router-link :to="pathTo">{{ text }} </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pathTo: { type: String },
+    text: { type: String },
+  },
+};
+</script>
+
+<style lang="scss" scoped src="./LinkAction.scss"></style>
