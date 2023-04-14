affe489a3e72d9546e48ded0ed6026fc1a28bac9
ioannis <ioannis.dimitriou@emark.com>
2023-04-14 11:07:54 +0200

    add-forgot-password-componet

---
+++ frontend/src/views/Account/ForgotPasswordView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="forgot-password">
+    <h1>Forgot Password</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style lang="scss" src="./LoginView.scss" scoped></style>
